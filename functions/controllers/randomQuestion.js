183af31b3454c5f953dfce656dd9a11adc163936
Gustavo Rodarte <gustavo.rodarte@frentecorretora.com.br>
2018-05-11 23:36:22 -0300

    Generate full quiz, and Encrypt Answer

--- functions/controllers/randomQuestion.js
+++ functions/controllers/randomQuestion.js
@@ -1,26 +1,55 @@
+require('dotenv').load();
 const functions = require('firebase-functions')
 const getLastReviewId = require('../database-controllers/lastReview')
 const getReviewById = require('../database-controllers/getReviewById')
+const shuffle = require('shuffle-array');
 
-const randomQuestion = (request, response) => {
+
+const CryptoJS = require("crypto-js");
+const Base64 = require('crypto-js/enc-base64')
+
+const quiz = (request, response) => {
+
+  return generateQuiz(request.query.amount).then(quiz => {
+    return response.send(quiz)
+  })
+}
+
+const generateQuiz = (amount = 10) => {
+
+  const arrPromises = Array.from({
+    length: amount
+  }).map(() => {
+    return generateRandomQuestion();
+  })
+
+  return Promise.all(arrPromises).then(quiz => quiz);
+
+}
+
+const generateRandomQuestion = () => {
 
   let answer;
-
   return randomReview()
     .then((result) => {
-      answer = result;
+      answer = result
       return generateAlternatives(answer)
     }).then((alternatives) => {
       alternatives.push(answer.company.toUpperCase())
-      return response.send({
-        answer,
-        alternatives
+      shuffle(alternatives)
+      return new Promise((resolve, reject) => {       
+        resolve({
+          comment:  answer.comment,
+          answer: encryptAnswer(answer.company.toUpperCase()),
+          alternatives
+        })
       })
-    })
+    }).catch(err => console.log(err))
+} 
+const encryptAnswer = (answer) => {
+  const encryptKey = process.env.ENCRYPTION_KEY || functions.config().encryption.key
+  return Base64.stringify(CryptoJS.HmacSHA1(answer, encryptKey))
 }
-
-//TODO
-const encryptAnswer= () => {}
 
 const randomReview = () => {
   return getLastReviewId()
@@ -31,31 +60,29 @@ const randomReview = () => {
     }).catch(err => console.log(err))
 }
 
-const generateAlternatives = (answer, amount = 3, result = []) => {
+const generateAlternatives = (answer, result = []) => {
 
   return randomReview()
     .then((reviewAlternative) => {
 
       const reviewAltCompany = reviewAlternative.company.toUpperCase()
 
-      const newResult = result.filter((alternative) => {
-        const condOne = alternative !== reviewAltCompany
-        const condTwo = alternative !== answer.company.toUpperCase()
+      let newResult = result.filter((alternative) => {
+        const isDifferentFromReview = alternative !== reviewAltCompany
+        const isDifferentFromAnswer = alternative !== answer.company.toUpperCase()
 
-        return condOne && condTwo
+        return isDifferentFromReview && isDifferentFromAnswer
       });
-      
-      
-      const newAmount = -1
+
 
       if (newResult.length === 3) return newResult
 
       newResult.push(reviewAltCompany);
 
-      return generateAlternatives(answer, newAmount, newResult)
+      return generateAlternatives(answer, newResult)
 
     }).catch(err => console.log(err))
 }
 
 
-module.exports = randomQuestion
+module.exports = quiz
