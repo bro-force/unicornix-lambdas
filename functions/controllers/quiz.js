4cb367fd7fec0490227c9be394be434f969fe2e2
Clayton Fidelis <clayton.fidelis@live.com>
2018-05-14 14:01:53 -0300

    feat(save-quiz): save quiz data

--- functions/controllers/quiz.js
+++ functions/controllers/quiz.js
@@ -2,10 +2,22 @@ require('dotenv').load();
 const functions = require('firebase-functions')
 const cors = require('cors')({ origin: true })
 
+const db = require('../database')
+
 const generateQuiz = require('../database-controllers/generateQuiz')
 
 const quiz = (request, response) => {
   return generateQuiz(request.query.amount)
+    .then(quiz => {
+      const quizId = db.ref('userQuizzes').push().key
+      db.ref(`userQuizzes/${quizId}/questions`).set(quiz)
+
+      return {
+        id: quizId,
+        questions: quiz,
+        nickname: request.query.nickname
+      }
+    })
     .then(quiz => cors(request, response, () => response.send(quiz)))
 }
 
